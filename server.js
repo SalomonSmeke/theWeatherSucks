503368cb2e99e500e5c0fa31b28f0b8de3f68c73
Salomon Smeke <ssmeke@luc.edu>
2016-04-16 17:21:02 -0500

    testing object printing

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ weatherSucks.get("/api/getLoc", function(req, res) {
     console.log(body);
     var old = {zip: "60660", country: "us", fetched: true};
     console.log(response);
-    console.log(JSON.parse(response));
+    console.log({zip: "60660", country: "us", fetched: true});
     res.send( response );
   });
 
