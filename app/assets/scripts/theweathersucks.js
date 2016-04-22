001fdf8324862d2c7ae46ee219f74002dba8d0d2
Salomon Smeke <ssmeke@luc.edu>
2016-04-21 23:55:51 -0500

    refactor

--- app/assets/scripts/theweathersucks.js
+++ app/assets/scripts/theweathersucks.js
@@ -1,7 +1,7 @@
 function load(){
   "use strict";
-  var defaultLoc = {zip: "60660", country: "us", state: "il", city: "chicago", fetched: false}
-  var defaultWeather = {zip: "60660", country: "us", state: "il", city: "chicago", fetched: false} //{type: type, desc: desc, temp: temp, tempMin: tempMin, tempMax: tempMax, humidity: humidity, wind: wind, fetched: true}
+
+
   function getLocation() {
     console.log("Location Request Started...")
     var geoOptions = {
@@ -14,6 +14,7 @@ function load(){
         locFail("GeoLocation: Not supported by browser or disabled");
     }
   }
+
   function locToZip(position) {
     var lat = (position.coords.latitude || "0") + "";
     var lon = (position.coords.longitude || "0") + "";
@@ -22,63 +23,37 @@ function load(){
       update(zipParse(response));
     });
   }
+
   function locFail(error) {
     console.error(error);
     update (defaultLoc);
   }
-  function zipParse(res) {
-    var state, city, zip;
-    //UGLY, but easy to read.
-    function unwrapZipRequest(input, search){
-     for (var result in input.results) {
-        for (var addrComp in input.results[result].address_components){
-          for (var type in input.results[result].address_components[addrComp].types){
-            if (input.results[result].address_components[addrComp].types[type]===search){
-              return input.results[result].address_components[addrComp].short_name;
-            }
-          }
-        }
-      }
-    }
-
-    state = unwrapZipRequest(res, "administrative_area_level_1") || null;
-    city = unwrapZipRequest(res, "locality") || null;
-    zip = unwrapZipRequest(res, "postal_code") || null;
-
-    if (city==null || state ==null || zip==null){
-      console.error("Failed to find location at those coordinates, using default.");
-      return(defaultLoc);
-    }
-    console.log("Successful location get: " + zip + " " + state + " " + city);
-    return {zip: zip, country: "us", state: state, city: city, fetched: true};
-  }
-
-/*
-var $deferredNotesRequest = $.getJSON (
-    "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
-    { tags: img_tags,
-      tagmode: "any",
-      format: "json"
-    });
-*/
-//http://openweathermap.org/current
 
   function update(loc) {
     var weather;
+
     var $deferredConditionsRequest = $.getJSON("/api/getCond?zip=" + loc.zip + "," + loc.country);
+
     $deferredConditionsRequest.then(function(value) {
       console.log("Response from server received.");
+
       weather = weatherParse(value);
-      if (weather.fetched){
-        console.log("Weather correctly fetched: " + JSON.stringify(weather))
+
+      if (weather){
+        console.log("Weather correctly fetched: " + JSON.stringify(weather));
       } else {
-        console.error("Weather fetch failed, using default.")
+        weather = defaultWeather;
+        console.error("Weather fetch failed, using default: " + JSON.stringify(weather));
       }
     }, function(reason) {
+
       console.error("frick" + reason);
       weather = defaultWeather;
+      
     });
+
     var worseWeather;
+
     // getWorseConditions(); //ASYNC D
     // setGreeting(); //ASYNC D
     // setIcon(); //ASYNC D
@@ -88,49 +63,6 @@ var $deferredNotesRequest = $.getJSON (
     // getMetrics(); //ASYNC E... but actually E. Cause lets do it last.
     //
     // drawMetricsD3(); //ASYNC F
-  }
-  function weatherParse(value){
-    var type, desc, temp, tempMin, tempMax, humidity, wind;
-
-    var flag = false;
-    if (value.weather !== undefined) {
-      if (value.weather[0].main !== undefined && value.weather[0].description !== undefined) {
-        type = value.weather[0].main;
-        desc = value.weather[0].description;
-      } else {
-        flag = true;
-      }
-    } else {
-      flag = true;
-    }
-
-    if (value.main !== undefined) {
-      if (value.main.temp !== undefined && value.main.temp_min !== undefined && value.main.temp_max !== undefined && value.main.humidity !== undefined) {
-        temp = value.main.temp;
-        tempMin = value.main.temp_min;
-        tempMax = value.main.temp_max;
-      } else {
-        flag = true;
-      }
-    } else {
-      flag = true;
-    }
-
-    if (value.wind !== undefined) {
-      if (value.wind.speed !== undefined) {
-        wind = value.wind.speed;
-      } else {
-        flag = true;
-      }
-    } else {
-      flag = true;
-    }
-
-    if (flag) {
-      return defaultWeather;
-    } else {
-      return {type: type, desc: desc, temp: temp, tempMin: tempMin, tempMax: tempMax, humidity: humidity, wind: wind, fetched: true};
-    }
   }
   //
   // function pickType() { //do this when conditions is done
---
+++ app/assets/scripts/utils.js
@@ -0,0 +1,70 @@
+function zipParse(res) {
+  var state, city, zip;
+
+  function unwrapZipRequest(input, search){
+   for (var result in input.results) {
+      for (var addrComp in input.results[result].address_components){
+        for (var type in input.results[result].address_components[addrComp].types){
+          if (input.results[result].address_components[addrComp].types[type]===search){
+            return input.results[result].address_components[addrComp].short_name;
+          }
+        }
+      }
+    }
+  }
+
+  state = unwrapZipRequest(res, "administrative_area_level_1") || null;
+  city = unwrapZipRequest(res, "locality") || null;
+  zip = unwrapZipRequest(res, "postal_code") || null;
+
+  if (city==null || state ==null || zip==null){
+    console.error("Failed to find location at those coordinates, using default.");
+    return(defaultLoc);
+  }
+  console.log("Successful location get: " + zip + " " + state + " " + city);
+  return {zip: zip, country: "us", state: state, city: city, fetched: true};
+}
+
+function weatherParse(value){
+  var type, desc, temp, tempMin, tempMax, humidity, wind;
+
+  var flag = false;
+  if (value.weather !== undefined) {
+    if (value.weather[0].main !== undefined && value.weather[0].description !== undefined) {
+      type = value.weather[0].main;
+      desc = value.weather[0].description;
+    } else {
+      flag = true;
+    }
+  } else {
+    flag = true;
+  }
+
+  if (value.main !== undefined) {
+    if (value.main.temp !== undefined && value.main.temp_min !== undefined && value.main.temp_max !== undefined && value.main.humidity !== undefined) {
+      temp = value.main.temp;
+      tempMin = value.main.temp_min;
+      tempMax = value.main.temp_max;
+    } else {
+      flag = true;
+    }
+  } else {
+    flag = true;
+  }
+
+  if (value.wind !== undefined) {
+    if (value.wind.speed !== undefined) {
+      wind = value.wind.speed;
+    } else {
+      flag = true;
+    }
+  } else {
+    flag = true;
+  }
+
+  if (flag) {
+    return false;
+  } else {
+    return {type: type, desc: desc, temp: temp, tempMin: tempMin, tempMax: tempMax, humidity: humidity, wind: wind, fetched: true};
+  }
+}
