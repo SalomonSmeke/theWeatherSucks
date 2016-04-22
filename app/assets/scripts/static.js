3a54f25bc053b8897564f99ce343d9eb7276efda
Salomon Smeke <ssmeke@luc.edu>
2016-04-22 00:04:47 -0500

    repairs to frozen objects

--- app/assets/scripts/static.js
+++ app/assets/scripts/static.js
@@ -1,8 +1,6 @@
-var locks;
-
 var defaultLoc = {zip: "60660", country: "us", state: "il", city: "chicago", fetched: false};
 var defaultWeather = {type: "Rain", desc: "light rain", temp: 286.43, tempMin: 286.43, tempMax: 286.43, wind: 3.11, fetched: false};
 var conditionsLookup = {cold: 0, neutral: 1, warm: 2};
 var conditionsRLookup = ["cold","neutral","warm"];
 
-locks[Object.freeze(defaultLoc),Object.freeze(defaultWeather),Object.freeze(conditionsLookup),Object.freeze(conditionsRLookup)];
+var locks = [Object.freeze(defaultLoc),Object.freeze(defaultWeather),Object.freeze(conditionsLookup),Object.freeze(conditionsRLookup)];
