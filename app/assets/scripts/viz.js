d5ebafb81d2565fa0f7d2e139dd5ab29396bd7e9
Salomon Smeke <ssmeke@luc.edu>
2016-04-25 01:06:24 -0500

    logic for getting colder places

--- app/assets/scripts/viz.js
+++ app/assets/scripts/viz.js
@@ -1,44 +1,80 @@
 function ChosenOfRNGesus(type,targetTemps) {
   var lookups;
-  function comparator;
-  var default;
+  var comparator;
+  var defaultWeathers;
   switch (type){
     case 3:
       lookups = pollingLocations.warm;
       comparator = function (a,b){
         return a < b;
       }
-      default = sunCopOut;
+      defaultWeathers = sunCopOut;
       break;
     default:
       lookups = pollingLocations.cold;
       comparator = function (a,b){
         return a > b;
       }
-      default = moonCopOut;
+      defaultWeathers = moonCopOut;
   }
-
-  var curr = getRNGesus(lookups);
-  var selected;
+  var indexSearch = getRNGesus(lookups);
+  var curr = lookups[indexSearch];
   $.getJSON("/api/getCondLatLon?lat=" + curr.lat + "&lon=" + curr.lon) //first try
   .then(
     function (res){
       res = weatherParse(res);
       if (comparator(targetTemps,res.temp)){
-        selected = res;
+        printMyDataYouBitch(res);
       } else {
-        lookups = lookups.splice(curr,1);
-        curr = getRNGesus(lookups);
-        $.getJSON("/api/getCondLatLon?lat=" + curr.lat + "&lon=" + curr.lon).then( //second try
+        lookups.splice(indexSearch,1);
+        indexSearch = getRNGesus(lookups);
+        curr = lookups[indexSearch];
+        $.getJSON("/api/getCondLatLon?lat=" + curr.lat + "&lon=" + curr.lon)
+        .then( //second try
           function (res){
             res = weatherParse(res);
             if (comparator(targetTemps,res.temp)){
-              selected = res;
+              printMyDataYouBitch(res);
             } else {
-              lookups = lookups.splice(curr,1);
-              curr = getRNGesus(lookups);
-              $.getJSON("/api/getCondLatLon?lat=" + curr.lat + "&lon=" + curr.lon).then(
-
+              lookups.splice(indexSearch,1);
+              indexSearch = getRNGesus(lookups);
+              curr = lookups[indexSearch];
+              $.getJSON("/api/getCondLatLon?lat=" + curr.lat + "&lon=" + curr.lon)
+              .then( //third
+                function (res){
+                  res = weatherParse(res);
+                  if (comparator(targetTemps,res.temp)){
+                    printMyDataYouBitch(res);
+                  } else {
+                    lookups.splice(indexSearch,1);
+                    indexSearch = getRNGesus(lookups);
+                    curr = lookups[indexSearch];
+                    $.getJSON("/api/getCondLatLon?lat=" + curr.lat + "&lon=" + curr.lon)
+                    .then( //fourth
+                      function (res){
+                        res = weatherParse(res);
+                        if (comparator(targetTemps,res.temp)){
+                          printMyDataYouBitch(res);
+                        } else {
+                          lookups.splice(indexSearch,1);
+                          indexSearch = getRNGesus(lookups);
+                          curr = lookups[indexSearch];
+                          $.getJSON("/api/getCondLatLon?lat=" + curr.lat + "&lon=" + curr.lon)
+                          .then( //last
+                            function (res) {
+                              res = weatherParse(res);
+                              if (comparator(targetTemps,res.temp)){
+                                printMyDataYouBitch(res);
+                              } else {
+                                printMyDataYouBitch(defaultWeathers);
+                              }
+                            }
+                          );
+                        }
+                      }
+                    )
+                  }
+                }
               )
             }
           }
@@ -46,16 +82,14 @@ function ChosenOfRNGesus(type,targetTemps) {
       }
     }
   );
-
 }
 
 function getRNGesus(arrayIn) {
   return Math.floor((Math.random() * arrayIn.length));
 }
 
-d3.select(window).on('resize', resize);
-
-function resize() {
-
-
+function printMyDataYouBitch(data) {
+  console.log(data);
 }
+
+ChosenOfRNGesus(1,10);
